6af4e25f0ac20611d1a617e3d72fa429af5b102a
박찬혁 <cksgurrhtn@naver.com>
2025-07-02 16:05:04 +0900

    memo_app commit

---
+++ src/test/java/com/chan7731/memo_app/MemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan7731.memo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
